08a40b56880744738f066d3caa108758bdd130fd
TonyPescy <tpescy16@gmail.com>
2025-08-08 13:23:07 -0400

    createdx fertlizer class

---
+++ fertilizer.py
@@ -0,0 +1,17 @@
+#######################################################################################################################################
+# Title: fertlizer
+# Author: Tony Pescatore
+# Description: fertilizer class to house important information about the crops.
+#######################################################################################################################################
+
+
+class Fertilizer:
+    # initialize basic fertilizer information
+    def __init__(self, name, price, season):
+        self.name = name
+        self.seed_price = price
+
+    
+def generate_fertilizer():
+    print('NAN')
+
